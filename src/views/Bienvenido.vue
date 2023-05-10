f73d72914ff1dda0c577f9c895bb456677d1a58e
josefidel-1998 <85585186+josefidel-1998@users.noreply.github.com>
2023-05-10 10:51:10 -0700

    d

--- src/views/Bienvenido.vue
+++ src/views/Bienvenido.vue
@@ -126,7 +126,7 @@ function logearse(datos) {
   }else{
     login(email, password)
     .then(datos => {
-      let id = datos._id;
+      //let id = datos._id;
       let token = datos.token;
 
       console.log(datos);
